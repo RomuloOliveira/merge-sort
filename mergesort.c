0386b2fd1469002b1250fb25ffb86b51ac2f685b
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2015-03-24 19:25:15 -0300

    Fix wrong middle calc

--- mergesort.c
+++ mergesort.c
@@ -7,7 +7,7 @@ void merge(int* list, int first_begin, int first_end, int second_begin, int seco
 }
 
 void merge_sort(int* list, int begin, int end) {
-    int middle = (begin - end) / 2;
+    int middle = (begin + end) / 2;
 
     if (begin - end < 1) {
         return;
